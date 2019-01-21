fd9144c3e88d5c06fdd422c52dc877a734ae685c
Karsten Thoms <karsten.thoms@itemis.de>
2019-01-21 17:05:14 +0100

    [eclipse/xtext#1384] Change Jenkinsfile to Declarative Pipeline
    - externalized build steps into shell scripts for convenient local usage
    - added Slack notifications
    - configure Maven default goal
    - parallelize Maven build and long-running tests
    
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

---
+++ 3-gradle-longrunning-tests.sh
@@ -0,0 +1,11 @@
+if [ -z "$JENKINS_URL" ]; then
+  # if not set in environment use default
+  JENKINS_URL=https://ci.eclipse.org/xtext/
+fi
+
+./gradlew \
+  longrunningTest \
+  -PuseJenkinsSnapshots=true \
+  -PJENKINS_URL=$JENKINS_URL \
+  -PignoreTestFailures=true \
+  $@
